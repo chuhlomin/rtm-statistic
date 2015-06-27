a01aaa1f8ac81b719dcd5dfcf62d1f149c9330e9
Konstantin Chukhlomin <chuhlomin@gmail.com>
2015-06-27 17:44:49 -0400

    Getting result per day

--- app/commands/TestCommand.php
+++ app/commands/TestCommand.php
@@ -4,6 +4,7 @@ namespace app\commands;
 
 
 use Rtm\Rtm;
+use Rtm\Service\Lists;
 use Rtm\Service\Tasks;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
@@ -32,13 +33,49 @@ class TestCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        /** @var Tasks $taskService */
-        $taskService = $this->rtm->getService(Rtm::SERVICE_TASKS);
-        $taskList = $taskService->getList();
+        /** @var Lists $listsService */
+        $listsService = $this->rtm->getService(Rtm::SERVICE_LISTS);
+
+        $lists = $listsService->getList();
+        $result = [];
+
+        $isInboxFound = false;
+        $listId = null;
+        foreach ($lists as $list) {
+            $name = $list->getName();
+            if ($name === 'Inbox') {
+                $isInboxFound = true;
+                $listId = $list->getId();
+                break;
+            }
+        }
+
+        if ($isInboxFound === false) {
+            throw new \LogicException('Inbox list not found');
+        }
+
+        $completed = $this->getCountPerListAndFilter($listId, 'completed:6/26/2015');
+        $added = $this->getCountPerListAndFilter($listId, 'added:6/26/2015');
+
+        $result = [
+            $completed,
+            $added
+        ];
 
         /** @var \Rtm\Service\Auth $rtmAuth */
-        $output->writeln(gettype($taskList));
-        $output->writeln(print_r($taskList, true));
+        $output->writeln(print_r($result, true));
+    }
 
+    /**
+     * @param $listId
+     * @param $filter
+     * @return int
+     */
+    private function getCountPerListAndFilter($listId, $filter)
+    {
+        /** @var Tasks $tasksService */
+        $tasksService = $this->rtm->getService(Rtm::SERVICE_TASKS);
+        $tasks = $tasksService->getList($filter, $listId)->toArray()[0];
+        return count($tasks['taskseries']);
     }
 }
