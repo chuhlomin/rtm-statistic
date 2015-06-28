4c383e8b20735d261852cc299318394fca74030c
Konstantin Chukhlomin <chuhlomin@gmail.com>
2015-06-28 00:29:07 -0400

    Just output

--- app/commands/TestCommand.php
+++ app/commands/TestCommand.php
@@ -7,6 +7,7 @@ use Rtm\Rtm;
 use Rtm\Service\Lists;
 use Rtm\Service\Tasks;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
@@ -28,16 +29,105 @@ class TestCommand extends Command
 
     protected function configure()
     {
-        $this->setName('test');
+        $this
+            ->setName('rtm:grabber')
+            ->addArgument('start', InputArgument::REQUIRED, 'Format: 2015-06-27')
+            ->addArgument('end', InputArgument::REQUIRED, 'Format: 2015-06-29 (including)')
+            ->addArgument('interval', InputArgument::OPTIONAL, '"1 day", "1 week" or something like this', '1 week');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $startInput = $input->getArgument('start');
+        $endInput = $input->getArgument('end');
+        $intervalInput = $input->getArgument('interval');
+
+        $listId = $this->getInboxListId();
+
+        $start = new \DateTime($startInput);
+        $end = new \DateTime($endInput);
+        $interval = \DateInterval::createFromDateString($intervalInput);
+        $period = new \DatePeriod($start, $interval, $end);
+
+        foreach ($period as $date) {
+            /** @var \DateTime $date */
+            $dateFormatted = $date->format('Y-m-d');
+            $nextPeriod = $date->add($interval);
+            $nextPeriodFormatted = $nextPeriod->format('Y-m-d');
+
+            $output->write($dateFormatted . "\t");
+
+            $tasks = $this->getTasksPerListAndFilter(
+                $listId,
+                sprintf('status:incomplete AND addedBefore:%s', $nextPeriodFormatted)
+            );
+
+            $lifeDays = 0;
+            foreach ($tasks as $task) {
+                $taskCreated = new \DateTime($task['created']);
+                $taskLife = $date->diff($taskCreated);
+
+                $taskLifeDays = (int) $taskLife->format('%a');
+                $lifeDays += $taskLifeDays;
+            }
+
+            $result = [
+                'completed' => $this->getCountPerListAndFilter(
+                    $listId,
+                    sprintf(
+                        'completedAfter:%s AND completedBefore:%s',
+                        $dateFormatted,
+                        $nextPeriodFormatted
+                    )
+                ),
+                'added' => $this->getCountPerListAndFilter(
+                    $listId,
+                    sprintf(
+                        'addedAfter:%s AND addedBefore:%s',
+                        $dateFormatted,
+                        $nextPeriodFormatted
+                    )
+                ),
+                'total' => count($tasks),
+                'days' => $lifeDays
+            ];
+
+            $output->writeln(implode("\t", array_values($result)));
+        }
+    }
+
+    /**
+     * @param $listId
+     * @param $filter
+     * @return int
+     */
+    private function getCountPerListAndFilter($listId, $filter)
+    {
+        return count($this->getTasksPerListAndFilter($listId, $filter));
+    }
+
+    /**
+     * @param $listId
+     * @param $filter
+     * @return array
+     */
+    private function getTasksPerListAndFilter($listId, $filter)
+    {
+        /** @var Tasks $tasksService */
+        $tasksService = $this->rtm->getService(Rtm::SERVICE_TASKS);
+        $tasks = $tasksService->getList($filter, $listId)->toArray()[0];
+        return $tasks['taskseries'];
+    }
+
+    /**
+     * @return int
+     */
+    private function getInboxListId()
     {
         /** @var Lists $listsService */
         $listsService = $this->rtm->getService(Rtm::SERVICE_LISTS);
 
         $lists = $listsService->getList();
-        $result = [];
 
         $isInboxFound = false;
         $listId = null;
@@ -54,28 +144,6 @@ class TestCommand extends Command
             throw new \LogicException('Inbox list not found');
         }
 
-        $completed = $this->getCountPerListAndFilter($listId, 'completed:6/26/2015');
-        $added = $this->getCountPerListAndFilter($listId, 'added:6/26/2015');
-
-        $result = [
-            $completed,
-            $added
-        ];
-
-        /** @var \Rtm\Service\Auth $rtmAuth */
-        $output->writeln(print_r($result, true));
-    }
-
-    /**
-     * @param $listId
-     * @param $filter
-     * @return int
-     */
-    private function getCountPerListAndFilter($listId, $filter)
-    {
-        /** @var Tasks $tasksService */
-        $tasksService = $this->rtm->getService(Rtm::SERVICE_TASKS);
-        $tasks = $tasksService->getList($filter, $listId)->toArray()[0];
-        return count($tasks['taskseries']);
+        return $listId;
     }
 }
